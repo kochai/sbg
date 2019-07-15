4673619ea8d249f0f29b584e6f2e91b8fe90f8de
Marko Kovacevic <mr.markokovacevic@gmail.com>
2019-07-15 15:25:11 +0200

    Remove angular-cli boilerplate code, Add module dependencies, Add components to App component and Modal functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { UserModel } from './models/user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'sbg-test';
+  public creationModalVisible = false;
+
+  constructor(public userModel: UserModel) { }
+
+  public toggleCreationModal(): void {
+    this.creationModalVisible = !this.creationModalVisible;
+  }
+
+  public closeModal(): void {
+    this.creationModalVisible = false;
+  }
 }
