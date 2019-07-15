cb350befaa439d8eabc378aa85c8c8218ce6eefe
Marko Kovacevic <mr.markokovacevic@gmail.com>
2019-07-15 15:40:02 +0200

    Fix e2e test

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to sbg-test!');
+    expect(page.getTitleText()).toEqual('Welcome, mr_markokovacevic!');
   });
 
   afterEach(async () => {
